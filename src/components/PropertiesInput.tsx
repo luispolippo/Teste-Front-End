4ebdf5dc574d044b7e11f382b60230dd69c326a2
Guilherme Polippo <luispolippo@hotmail.com>
2022-05-19 20:41:27 -0300

    feat: Cria função showCnpj
    Cria função para mostrar cnpj quando propriedade for selecionada

--- src/components/PropertiesInput.tsx
+++ src/components/PropertiesInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
 import MenuItem from '@mui/material/MenuItem';
 import Properties from '../interfaces/Properties';
 
@@ -11,24 +12,40 @@ type PropertiesInputProps = {
 
 function PropertiesInput(props: PropertiesInputProps) {
   const { data, properties, setProperties } = props;
+
+  const showCNPJ = () => {
+    if (properties) {
+      const propertiesJson = JSON.parse(properties);
+      return (
+        <FormHelperText>{propertiesJson.cnpj}</FormHelperText>
+      );
+    }
+    return false;
+  };
+
   return (
-    <TextField
-      select
-      label="Propriedades"
-      variant="standard"
-      fullWidth
-      value={properties}
-      onChange={(e) => { setProperties(e.target.value); }}
-      required
-    >
+    <>
+      <TextField
+        select
+        label="Propriedades"
+        variant="standard"
+        fullWidth
+        value={properties}
+        onChange={(e) => { setProperties(e.target.value); }}
+        required
+      >
+        {
+          data.map((property) => (
+            <MenuItem key={property.id} value={JSON.stringify(property)}>
+              {property.property}
+            </MenuItem>
+          ))
+        }
+      </TextField>
       {
-        data.map((property) => (
-          <MenuItem key={property.id} value={JSON.stringify(property)}>
-            {property.property}
-          </MenuItem>
-        ))
+        showCNPJ()
       }
-    </TextField>
+    </>
   );
 }
 
