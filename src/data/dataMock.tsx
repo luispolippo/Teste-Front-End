6a9916fe69867dec3bb8693ae34dbcb8b1ef08a5
Guilherme Polippo <luispolippo@hotmail.com>
2022-05-19 15:57:09 -0300

    feat: Cria mocks para alimentar os selects

---
+++ src/data/dataMock.tsx
@@ -0,0 +1,40 @@
+export interface PropertiesSelect {
+  property: string,
+  cnpj: string,
+}
+
+const propertiesSelectMock: PropertiesSelect[] = [
+  {
+    property: 'Agrotis 1',
+    cnpj: '04.909.987/0001-89',
+  },
+  {
+    property: 'Agrotis 2',
+    cnpj: '04.909.987/0001-88',
+  },
+  {
+    property: 'Agrotis 3',
+    cnpj: '04.909.987/0001-87',
+  },
+  {
+    property: 'Agrotis 4',
+    cnpj: '04.909.987/0001-86',
+  },
+  {
+    property: 'Agrotis 5',
+    cnpj: '04.909.987/0001-85',
+  },
+];
+
+const laboratoriesSelectMock = [
+  'Agro Skynet',
+  'Umbrella Agro',
+  'Osbourn Agro',
+  'Skyrim Agro',
+  'Agro Brasil',
+];
+
+export {
+  propertiesSelectMock,
+  laboratoriesSelectMock,
+};
