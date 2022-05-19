240f2d4b03ec707646f04a04b5363af525549936
Guilherme Polippo <luispolippo@hotmail.com>
2022-05-19 19:21:29 -0300

    feat: Implementa controle dos componentes de formulário

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,27 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
+import React from 'react';
 import { DesktopDatePicker } from '@mui/x-date-pickers';
 import TextField from '@mui/material/TextField';
 
 type DateInputsProps = {
-  label: string;
+  label: string,
+  date: Date | string | null,
+  setDate: (newDate: Date | string | null) => void
 };
 
 function DateInputs(props: DateInputsProps) {
-  const { label } = props;
+  const { label, date, setDate } = props;
 
-  const [date, setDate] = useState<Date | null>(null);
-
-  const handleDateChange = (newDate: Date | null) => {
-    setDate(newDate);
+  const convertDate = (longDate: Date | null): string | null => {
+    if (longDate) return longDate.toISOString();
+    return null;
   };
 
   return (
     <DesktopDatePicker
       label={label}
-      inputFormat="dd/MM/yyyy"
       value={date}
-      onChange={handleDateChange}
+      onChange={(newDate: Date | null) => {
+        const convertedDate = convertDate(newDate);
+        setDate(convertedDate);
+      }}
+      inputFormat="dd/MM/yyyy"
       renderInput={(params) => <TextField {...params} required variant="standard" fullWidth />}
     />
   );
--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
-import { PropertiesSelect } from '../data/dataMock';
+import Properties from '../interfaces/Properties';
 
 type SelectInputProps = {
-  label: string
-  data: PropertiesSelect[] | string[]
+  label: string,
+  data: Properties[] | string[],
+  selectValue: Properties | string,
+  setSelectValue: (newProperty: Properties | string) => void,
 };
 
 function SelectInput(props: SelectInputProps) {
-  const { label, data } = props;
+  const {
+    label, data, selectValue, setSelectValue,
+  } = props;
   return (
     <TextField
       select
@@ -17,19 +21,21 @@ function SelectInput(props: SelectInputProps) {
       variant="standard"
       fullWidth
       required
+      value={selectValue}
+      onChange={(e) => { setSelectValue(e.target.value); }}
     >
       {
-        data.map((value) => {
-          if (typeof value === 'string') {
-            return <MenuItem key={value} value={value}>{value}</MenuItem>;
-          }
-          return (
-            <MenuItem key={value.cnpj} value={value.property}>
-              {value.property}
-            </MenuItem>
-          );
-        })
-      }
+          data.map((value) => {
+            if (typeof value === 'string') {
+              return <MenuItem key={value} value={value}>{value}</MenuItem>;
+            }
+            return (
+              <MenuItem key={value.cnpj} value={value.property}>
+                {value.property}
+              </MenuItem>
+            );
+          })
+        }
     </TextField>
   );
 }
